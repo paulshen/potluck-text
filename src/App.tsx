af08f779b31041bab81d0ba60f088a14a49b79ee
Paul Shen <paulshen0@gmail.com>
2022-06-24 14:55:53 -0700

    setup codemirror

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+import { useEffect, useRef } from "react";
+import { EditorView, basicSetup } from "codemirror";
+import { highlightSelectionMatches } from "@codemirror/search";
+
 export function App() {
-  return <div>hi!</div>;
+  const editorRef = useRef(null);
+  useEffect(() => {
+    const view = new EditorView({
+      extensions: [
+        basicSetup,
+        EditorView.theme({
+          "&": { height: "100%" },
+        }),
+        highlightSelectionMatches({ wholeWords: true }),
+      ],
+      parent: editorRef.current!,
+    });
+    return () => {
+      view.destroy();
+    };
+  }, []);
+  return (
+    <div className="p-8">
+      <div className="w-[512px] h-[512px]" ref={editorRef}></div>
+    </div>
+  );
 }
