9e8dcfadc1272ecc8c86563cf7c904928936ad7c
Paul Shen <paulshen0@gmail.com>
2022-06-25 08:54:13 -0700

    cmd-click range

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
 import { useEffect, useRef } from "react";
-import { EditorView, basicSetup } from "codemirror";
+import { EditorView, minimalSetup } from "codemirror";
 import { highlightSelectionMatches } from "@codemirror/search";
+import { ViewPlugin, ViewUpdate } from "@codemirror/view";
+
+const plugin = ViewPlugin.fromClass(
+  class {
+    lastUpdate: any;
+    constructor(view: EditorView) {}
+    update(update: ViewUpdate) {
+      this.lastUpdate = update;
+    }
+    destroy() {}
+  },
+  {
+    eventHandlers: {
+      mousedown(event, view) {
+        if (event.metaKey) {
+          console.log(this.lastUpdate, event, view);
+          const pos = view.posAtCoords({ x: event.clientX, y: event.clientY });
+          if (pos === null) {
+            return;
+          }
+          for (const range of view.state.selection.ranges) {
+            if (pos >= range.from && pos < range.to) {
+              console.log(view.state.sliceDoc(range.from, range.to));
+              console.log(view.coordsAtPos(range.from));
+            }
+          }
+          return true;
+        }
+      },
+    },
+  }
+);
 
 export function App() {
   const editorRef = useRef(null);
   useEffect(() => {
     const view = new EditorView({
       extensions: [
-        basicSetup,
+        minimalSetup,
         EditorView.theme({
           "&": { height: "100%" },
         }),
-        highlightSelectionMatches({ wholeWords: true }),
+        EditorView.lineWrapping,
+        plugin,
       ],
       parent: editorRef.current!,
     });
