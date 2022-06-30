a868878e64f90ee6b6bf37a47c6051e21c7da041
Paul Shen <paulshen0@gmail.com>
2022-06-30 15:25:56 -0700

    overflow-ellipsis token

--- src/Token.tsx
+++ src/Token.tsx
@@ -10,7 +10,7 @@ export function Token({
   return (
     <div
       className={classNames(
-        "relative px-2 py-1 text-xs font-mono rounded cursor-default whitespace-nowrap",
+        "relative px-2 py-1 text-xs font-mono rounded cursor-default whitespace-nowrap overflow-hidden text-ellipsis",
         isSelected ? "shadow-lg bg-zinc-300" : "bg-zinc-200"
       )}
     >
